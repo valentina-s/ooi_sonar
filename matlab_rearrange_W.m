cb4bef495dfa68d77e6563aa18c322f69885730e
Wu-Jung Lee <leewujung@gmail.com>
2017-11-29 18:09:27 -0800

    rearranging seems to be correct

--- matlab_rearrange_W.m
+++ matlab_rearrange_W.m
@@ -1,9 +1,14 @@
 % 2017 11 28  Rename files to form better sequence
 
 clear
-addpath ~/internal_2tb/ooi_sonar/ooi_sonar_code/
 
-data_path = '/media/wu-jung/wjlee_apl_2/ooi_zplsc_new/';
+if ismac
+    data_path = '/Users/wujung/Downloads/';
+    addpath /Users/wujung/code/ooi_sonar/
+else
+    data_path = '/media/wu-jung/wjlee_apl_2/ooi_zplsc_new/';
+    addpath ~/internal_2tb/ooi_sonar/ooi_sonar_code/
+end
 
 files = dir(fullfile(data_path,'palm_*.mat'));
 
@@ -11,11 +16,11 @@ files = dir(fullfile(data_path,'palm_*.mat'));
 idx_match = nan(4,2);
 idx_match_all = nan(length(files),4);
 fig = figure('position',[100,200,500,600]);
-for iF=1:10%length(files)
+for iF=1:length(files)
     A = load(fullfile(data_path,files(iF).name));
 
     s = strsplit(files(iF).name,'.mat');
-    num = str2num(s{1}(13:end));
+    eta = str2double(s{1}(13:end));
 
     if iF~=1
         RHO = corr(W_prev,A.W);
@@ -41,21 +46,22 @@ for iF=1:10%length(files)
     V = reshape(W_prev,37,144*3,4);
 
     figure(fig);
+%     figure;
     for iC=1:4
         subplot(6,1,iC)
         imagesc(V(:,:,iC))
         axis xy
         colorbar
         if iC==1
-            title(['eta=',num])
+            title(['eta=',num2str(eta)])
         end
         hold on
         plot([144,144],[0,37],'w--','linewidth',1);
         plot([288,288],[0,37],'w--','linewidth',1);
     end
     subplot(6,1,5:6)
-    plot(A.H(idx_match,:)','linewidth',2);
+    plot(A.H(idx_match(:,2),:)','linewidth',2);
     legend('1','2','3','4');
 
-    saveas(gcf,fullfile(data_path,sprintf('palm_nmf_eta%010.1f.png',num)),'png');
+    saveas(gcf,fullfile(data_path,sprintf('palm_nmf_eta%010.1f.png',eta)),'png');
 end
