91a5a4d94d34c4e84ff43ad14c66709cfcb61620
Wu-Jung Lee <leewujung@gmail.com>
2017-12-07 14:17:44 -0800

    rearrange W by corrcoef to compare across diff params

--- matlab_rearrange_W.m
+++ matlab_rearrange_W.m
@@ -10,21 +10,22 @@ else
     addpath ~/internal_2tb/ooi_sonar/ooi_sonar_code/
 end
 
-files = dir(fullfile(data_path,'palm_*.mat'));
+files = dir(fullfile(data_path,'palm_nmf_r3_*.mat'));
 
+A = load(fullfile(data_path,files(1).name));
 
-idx_match = nan(4,2);
-idx_match_all = nan(length(files),4);
+idx_match = nan(size(A.W,2),2);
+idx_match_all = nan(length(files),size(A.W,2));
 fig = figure('position',[100,200,500,600]);
 for iF=1:length(files)
     A = load(fullfile(data_path,files(iF).name));
 
     s = strsplit(files(iF).name,'.mat');
-    eta = str2double(s{1}(13:end));
+    eta = str2double(s{1}(16:end));
 
     if iF~=1
         RHO = corr(W_prev,A.W);
-        for iC=1:4
+        for iC=1:size(A.W,2)
             [~,idx] = max(RHO(:));
             [idx_match(iC,1),idx_match(iC,2)] = ind2sub(size(RHO),idx);
             RHO(idx_match(iC,1),:) = -Inf;
@@ -32,8 +33,8 @@ for iF=1:length(files)
         end
         idx_match = sortrows(idx_match);
     else
-        idx_match(:,1) = [1,2,3,4];
-        idx_match(:,2) = [1,2,3,4];
+        idx_match(:,1) = [1:size(A.W,2)]';
+        idx_match(:,2) = [1:size(A.W,2)]';
     end
     idx_match_all(iF,:) = idx_match(:,2);
     W_prev = A.W(:,idx_match(:,2));
@@ -41,14 +42,16 @@ for iF=1:length(files)
     W1_all(:,iF) = W_prev(:,1);
     W2_all(:,iF) = W_prev(:,2);
     W3_all(:,iF) = W_prev(:,3);
-    W4_all(:,iF) = W_prev(:,4);
+    if size(A.W,2)==4
+        W4_all(:,iF) = W_prev(:,4);
+    end
 
-    V = reshape(W_prev,37,144*3,4);
+    V = reshape(W_prev,37,144*3,size(A.W,2));
 
     figure(fig);
 %     figure;
-    for iC=1:4
-        subplot(6,1,iC)
+    for iC=1:size(A.W,2)
+        subplot(size(A.W,2)+2,1,iC)
         imagesc(V(:,:,iC))
         axis xy
         colorbar
@@ -59,9 +62,9 @@ for iF=1:length(files)
         plot([144,144],[0,37],'w--','linewidth',1);
         plot([288,288],[0,37],'w--','linewidth',1);
     end
-    subplot(6,1,5:6)
+    subplot(size(A.W,2)+2,1,size(A.W,2)+(1:2))
     plot(A.H(idx_match(:,2),:)','linewidth',2);
     legend('1','2','3','4');
 
-    saveas(gcf,fullfile(data_path,sprintf('palm_nmf_eta%010.1f.png',eta)),'png');
+    saveas(gcf,fullfile(data_path,sprintf('palm_nmf_eta%06.2f.png',eta)),'png');
 end
