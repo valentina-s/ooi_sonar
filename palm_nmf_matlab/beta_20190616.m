c46a16a59944f3ac495d41261455787bee601882
leewujung <leewujung@gmail.com>
2019-06-21 01:22:23 -0700

    add code: run ss-NMF and check results with varying betaH and betaW

---
+++ palm_nmf_matlab/beta_20190616.m
@@ -0,0 +1,75 @@
+% 2019 04 26  Search for parameter combination that minimizes objective
+
+
+% Set paths
+if ismac && isunix  % if on Mac
+    addpath /Users/wu-jung/code_git/ooi_sonar/palm_nmf_matlab
+    data_path = '/Users/wu-jung/code_git/ooi_sonar/sample_data/';
+    save_path = '/User/wu-jung/code_git/ooi_sonar/decomp_smoothness_revisit_2e4';
+elseif isunix  % if on Linux
+    addpath ~/code_git/ooi_sonar/palm_nmf_matlab
+    data_path = '~/code_git/ooi_sonar/sample_data/';
+    save_path = '/media/wu-jung/internal_2tb/nmf_results/decomp_beta_revisit_1e4';
+end
+data_file = '20150817-20151017_MVBS_PCPcleaned.h5';
+
+cd(data_path)  % has to switch folder to ensure reading h5 files correctly
+
+
+% Get current script name
+pname = mfilename('fullpath');
+[~,sname,~] = fileparts(pname);  % script name
+
+
+% Load PCP-cleaned data
+L = h5read(data_file,'/L');
+L_sep = h5read(data_file,'/L_sep');
+L_plot = h5read(data_file,'/L_plot');
+
+depth_bin_size = h5read(data_file,'/depth_bin_size');
+ping_time = h5read(data_file,'/ping_time');
+ping_per_day_mvbs = h5read(data_file,'/ping_per_day_mvbs');
+depth_bin_num = 37;
+
+
+LL = L-min(L(:));  % make it non-negative
+
+
+% Run smooth NMF, sweep through different smoothness
+sm_all = [1e2,1e4,1e6,1e8];
+
+len = length(sm_all);
+f1 = 'r';
+v1 = mat2cell(3*ones(len,1),ones(len,1));
+f2 = 'max_iter';
+v2 = mat2cell(1e4*ones(len,1),ones(len,1));
+f3 = 'betaW';
+v3 = mat2cell(0*ones(len,1),ones(len,1));
+f4 = 'betaH';
+v4 = mat2cell(0*ones(len,1),ones(len,1));
+f5 = 'smoothness';
+v5 = mat2cell(sm_all',ones(len,1));
+params_all = struct(f1,v1,f2,v2,f3,v3,f4,v4,f5,v5);
+
+
+parfor rr = 1:length(sm_all)
+    fprintf('smoothness = %d\n', sm_all(rr));
+    [W, H, objective, iter_times, W_init, H_init, W_steps, H_steps] = ...
+        palm_nmf_detail(LL, params_all(rr));
+    save_file = ...
+        sprintf('%s_r%02d_betaW%2.2f_betaH%2.2f_smoothness%09.2f.mat', ...
+        sname, params_all(rr).r, ...
+        params_all(rr).betaW, params_all(rr).betaH, ...
+        params_all(rr).smoothness);
+    save_file = fullfile(save_path, save_file);
+    m = matfile(save_file,'writable',true);
+    m.W = W;
+    m.H = H;
+    m.W_init = W_init;
+    m.H_init = H_init;
+    m.W_steps = W_steps;
+    m.H_steps = H_steps;
+    m.objective = objective;
+    m.iter_times = iter_times;
+    m.params = params_all(rr);
+end
---
+++ palm_nmf_matlab/check_beta.m
@@ -0,0 +1,79 @@
+% 2019 06 14
+
+filepath = '/media/wu-jung/internal_2tb/nmf_results/decomp_beta_revisit_1e4/';
+smoothness = 1e8;
+A_str = '1e8';
+
+switch smoothness
+    case 1e2        
+        A = load(fullfile(filepath,'beta_20190616_r03_betaW0.00_betaH0.00_smoothness000100.00.mat'));
+    case 1e4
+        A = load(fullfile(filepath,'beta_20190616_r03_betaW0.00_betaH0.00_smoothness010000.00.mat'));
+    case 1e6
+        A = load(fullfile(filepath,'beta_20190616_r03_betaW0.00_betaH0.00_smoothness1000000.00.mat'));
+    case 1e8
+        A = load(fullfile(filepath,'beta_20190616_r03_betaW0.00_betaH0.00_smoothness100000000.00.mat'));
+end
+
+comps = [1,2,3];
+step_str = {'1e2','2e2','5e2','1e3','2e3','1e4'};
+
+for istep = 1:length(step_str)
+    step = eval(step_str{istep});
+
+    % H at different steps
+    H = squeeze(A.H_steps(:,:,step));
+    for icomp=1:length(comps)
+        H_lines(icomp,:,istep) = H(icomp,:);
+    end
+    
+    % W at different steps
+    fig = figure;
+    W = squeeze(A.W_steps(:,:,step));
+    V = zeros(37,144*3,3);
+    for icomp = 1:length(comps)
+        V = reshape(W(:,icomp),37,144*3);
+        V = V/max(max(V));
+        subplot(length(comps),1,icomp)
+        imagesc(V)
+        caxis([0,1])
+        colorbar
+        if icomp==1
+            title(['smoothness = ',num2str(smoothness),...
+                   ', step = ',num2str(step),...
+                   ', betaH=betaW=0'])
+        end
+    end
+    saveas(gcf,fullfile(filepath,...
+                        sprintf('A%s_W_step%05d.png',A_str,step)));
+end
+
+% Plot H
+for icomp=1:3
+    figure
+    plot(squeeze(H_lines(icomp,:,:)));
+    title(sprintf('smoothness%s, betaH=betaW=0, comp #%d',...
+                  A_str, icomp));
+    legend(step_str)
+    saveas(gcf,fullfile(filepath,sprintf('A%s_H_comp%d.png',A_str,icomp)));
+end
+
+% Plot objective
+figure
+plot(A.objective);
+ylabel('Objective');
+xlabel('Steps')
+title(sprintf('smoothness%s, betaH=betaW=0',A_str));
+if strcmp(A_str,'1e2')
+    ylim([1.75,1.85]*1e6)
+elseif strcmp(A_str,'1e4')
+    ylim([1.5 2]*1e6)
+elseif strcmp(A_str,'1e6')
+    ylim([1,3]*1e6)
+elseif strcmp(A_str,'1e8')
+    ylim([4,8]*1e6)
+end
+grid
+saveas(gcf,fullfile(filepath,sprintf('A%s_objective.fig',A_str)));
+saveas(gcf,fullfile(filepath,sprintf('A%s_objective.png',A_str)));
+
