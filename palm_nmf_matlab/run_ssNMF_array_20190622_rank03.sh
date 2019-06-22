07ff2517454d4642545540ae580ca14ab36c9516
Shao-shan Carol Huang <shhuang1@gmail.com>
2019-06-22 16:31:20 -0400

    add code: shell scripts to run 3 different rank with smoothness and sparsity sweep

---
+++ palm_nmf_matlab/run_ssNMF_array_20190622_rank03.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This script runs multiple single core Matlab jobs using
+# a slurm job array 
+
+#SBATCH --array=1-72
+#SBATCH --time=16:00:00
+#SBATCH --mem=6GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=log/slurm_%A-%a.out
+#SBATCH --error=log/slurm_%A-%a.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=leewujung@gmail.com
+
+# Load Matlab environment
+module load matlab/2018b
+
+# Create a temporary directory on scratch for any Job related files
+slurmArrayID="${SLURM_ARRAY_JOB_ID}${SLURM_ARRAY_TASK_ID}"
+export slurmArrayID
+mkdir -p /scratch/ch153/wjl/slurmJobs/$slurmArrayID
+
+PARAM_FILE="'/scratch/ch153/wjl/params_20190622_rank03.txt'"
+matlab -nodisplay -nosplash -singleCompThread -r "ssNMF_runner($PARAM_FILE, $SLURM_ARRAY_TASK_ID);exit;"
+
+# remove workspace
+rm -rf /scratch/ch153/wjl/slurmJobs/$slurmArrayID
