4948eb0f86099e8c22276b26d7614fbfc7be9ea7
leewujung <leewujung@gmail.com>
2019-06-23 10:20:02 +0200

    modify generate_ssNMF_param_file to generate files with different rank

--- palm_nmf_matlab/generate_ssNMF_param_file.m
+++ palm_nmf_matlab/generate_ssNMF_param_file.m
@@ -1,34 +1,36 @@
 % Test writing and reaiding formatted txt files
 
 save_path = '/scratch/ch153/wjl/';
-save_file = 'params_20190622_rank08.txt';
 
-rank = 8;          % rank
-betaH = 0.1;
-betaW = 0.1;
-sp = [1,2,5,10,20,50];  % sparsity (lambda)
-max_iter = 2e4;    % max iteration
+rank_all = 3:10;   % rank
 
-% smoothness
-sm_order = [5,6,7,8];
-sm = repmat([1,2,5],length(sm_order),1);
-for iorder = 1:length(sm_order)
-    sm(iorder,:) = sm(iorder,:)*10^sm_order(iorder);
-end
-sm = sm';
-sm = sm(:);
-
-% write text file
-fileID = fopen(fullfile(save_path, save_file),'w');
-fprintf(fileID,'%4s %5s %5s %9s %9s %9s\n','rank','betaH','betaW','sparsity','smoothness','max_iter');
-for isp=1:length(sp)
-    for ism=1:length(sm)
-        fprintf(fileID,'%4d %05.2f %05.2f %09.2e %09.2e %09.2e\n',rank, betaH, betaW, sp(isp), sm(ism), max_iter);
+for irank=1:length(rank_all)
+    
+    r = rank_all(irank);
+    save_file = sprintf('params_20190622_rank%02d.txt',r);
+    
+    betaH = 0.1;
+    betaW = 0.1;
+    sp = [1,2,5,10,20,50];  % sparsity (lambda)
+    max_iter = 2e4;    % max iteration
+    
+    % smoothness
+    sm_order = [5,6,7,8];
+    sm = repmat([1,2,5],length(sm_order),1);
+    for iorder = 1:length(sm_order)
+        sm(iorder,:) = sm(iorder,:)*10^sm_order(iorder);
     end
+    sm = sm';
+    sm = sm(:);
+    
+    % write text file
+    fileID = fopen(fullfile(save_path, save_file),'w');
+    fprintf(fileID,'%4s %5s %5s %9s %9s %9s\n','rank','betaH','betaW','sparsity','smoothness','max_iter');
+    for isp=1:length(sp)
+        for ism=1:length(sm)
+            fprintf(fileID,'%4d %05.2f %05.2f %09.2e %09.2e %09.2e\n',r, betaH, betaW, sp(isp), sm(ism), max_iter);
+        end
+    end
+    fclose(fileID);
+    
 end
-fclose(fileID);
-
-% % read text file
-% fileID = fopen(fullfile(save_path,save_file));
-% C = textscan(fileID,'%d %f %f %f %f','HeaderLines',1);
-% fclose(fileID);
